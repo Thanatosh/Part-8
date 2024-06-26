64a29bcbe18afa9841efa58d5ae60df9765f31b9
Thanatos <Thanatosh@gmail.com>
2024-06-26 09:06:56 +0300

    Part 8.6 Full Commit

--- Part-8A-Backend/App.js
+++ Part-8A-Backend/App.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -101,6 +102,15 @@ const typeDefs = `
     born: Int
     bookCount: Int!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+}
 `
 
 const resolvers = {
@@ -121,6 +131,22 @@ const resolvers = {
   },
   Author: {
     bookCount: (parent) => books.filter(book => book.author === parent.name).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const authorExists = authors.find(author => author.name === args.author)
+      if (!authorExists) {
+        const newAuthor = {
+          name: args.author,
+          id: uuid(),
+          born: args.born || null,
+        }
+        authors = authors.concat(newAuthor);
+      }
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
