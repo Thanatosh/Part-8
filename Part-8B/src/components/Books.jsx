105a23e07e0faab3722aa6769fe59af04f620aee
Thanatos <Thanatosh@gmail.com>
2024-06-28 09:16:15 +0300

    Part 8.8-9 Full Commit

---
+++ Part-8B/src/components/Books.jsx
@@ -0,0 +1,47 @@
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`;
+
+const Books = (props) => {
+  const { loading, error, data } = useQuery(ALL_BOOKS); 
+
+  if (!props.show) {
+    return null
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Published</th>
+          </tr>
+          {data.allBooks.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+};
+
+export default Books;
